18e54dd91611527948402e550da1c50fa853796f
meirelesDiogo <didialexandre1017@gmail.com>
2025-10-04 20:58:21 -0300

    Adicionei página principal com links das aulas

--- aula 1/script.js
+++ aula 1/script.js
@@ -1,4 +1,5 @@
-let nome = prompt("Digite Seu Nome")
-   function mudarNome(){
-    document.getElementById("memsagemSaudacao").textContent="Olá, " + nome
-   }
+function mudarNome(){
+   // pede o nome só quando o botão for clicado
+   const nome = prompt("Digite seu nome");
+   document.getElementById("mensagemSaudacao").textContent="Olá," + nome;
+}
